bf231c8e1dfde8b479a5fa7ae0884edfb2fecfba
maahd <maahdsh@gmail.com>
2019-08-26 02:04:26 +0200

    Set up project and display models

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,28 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Maahd Shahzad on 8/25/19.
+//  Copyright © 2019 Maahd Shahzad. All rights reserved.
+//
+
+import Foundation
+
+struct ToDo {
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String?
+    
+    static func loadToDos() -> [ToDo]? {
+        return nil
+    }
+    
+    static func loadSampleToDos() -> [ToDo] {
+        let todo1 = ToDo(title: "ToDo One", isComplete: false, dueDate: Date(), notes: "Notes 1")
+        let todo2 = ToDo(title: "ToDo Two", isComplete: false, dueDate: Date(), notes: "Notes 2")
+        let todo3 = ToDo(title: "ToDo Three", isComplete: false, dueDate: Date(), notes: "Notes 3")
+        
+        return [todo1, todo2, todo3]
+    }
+}
